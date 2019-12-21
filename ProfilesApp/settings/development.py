025055ff7edbdeee853fe92d4e27a20e7d6120e0
mohamed belkahla <mohamadbr22@gmail.com>
2019-12-21 11:59:55 +0100

    c2

--- ProfilesApp/settings/development.py
+++ ProfilesApp/settings/development.py
@@ -36,8 +36,12 @@ DEBUG_TOOLBAR_CONFIG = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "profiles",
+        'USER': "root",
+        'PASSWORD': "",
+        'HOST': "localhost",
+        'PORT': '3306'
     }
 }
 
